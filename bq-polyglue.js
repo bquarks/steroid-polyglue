a8e9efa688494991a28b4577a7f6ad74926f6884
Jorge Serrano <jorge.serrano@bq.com>
2015-06-24 15:29:45 +0200

    first commit

---
+++ bq-polyglue.js
@@ -0,0 +1,126 @@
+(function(Polymer) {
+
+    'use strict';
+
+    var Dispatcher = Polymer({
+
+        is: 'bq-gluon',
+        _prefix: 'ID_',
+        created: function() {
+            this._lastID = 1;
+            this._callbacks = {};
+            this._isPending = {};
+            this._isHandled = {};
+            this._isDispatching = false;
+            this._pendingPayload = null;
+        },
+        /**
+         * Registers a callback to be invoked with every dispatched payload. Returns
+         * a token that can be used with `waitFor()`.
+         *
+         * @param {function} callback
+         * @return {string}
+         */
+        register(callback) {
+            var id = this._prefix + this._lastID++;
+            this._callbacks[id] = callback;
+            return id;
+        },
+        /**
+         * Removes a callback based on its token.
+         *
+         * @param {string} id
+         */
+        unregister(id) {
+            delete this._callbacks[id];
+        },
+        /**
+         * Waits for the callbacks specified to be invoked before continuing execution
+         * of the current callback. This method should only be used by a callback in
+         * response to a dispatched payload.
+         *
+         * @param {array<string>} ids
+         */
+        waitFor(ids) {
+
+            for (var ii = 0; ii < ids.length; ii++) {
+                var id = ids[ii];
+                if (this._isPending[id]) {
+
+                    continue;
+                }
+
+                this._invokeCallback(id);
+            }
+        },
+        /**
+         * Dispatches a payload to all registered callbacks.
+         *
+         * @param {object} payload
+         */
+        dispatch(payload) {
+
+            this._startDispatching(payload);
+            try {
+                for (var id in this._callbacks) {
+                    if (this._isPending[id]) {
+                        continue;
+                    }
+                    this._invokeCallback(id);
+                }
+            } finally {
+                this._stopDispatching();
+            }
+        },
+
+        /**
+         * Is this Dispatcher currently dispatching.
+         *
+         * @return {boolean}
+         */
+        isDispatching() {
+            return this._isDispatching;
+        },
+
+        /**
+         * Call the callback stored with the given id. Also do some internal
+         * bookkeeping.
+         *
+         * @param {string} id
+         * @internal
+         */
+        _invokeCallback(id) {
+            this._isPending[id] = true;
+            this._callbacks[id](this._pendingPayload);
+            this._isHandled[id] = true;
+        },
+
+        /**
+         * Set up bookkeeping needed when dispatching.
+         *
+         * @param {object} payload
+         * @internal
+         */
+        _startDispatching(payload) {
+            for (var id in this._callbacks) {
+                this._isPending[id] = false;
+                this._isHandled[id] = false;
+            }
+            this._pendingPayload = payload;
+            this._isDispatching = true;
+        },
+
+        /**
+         * Clear bookkeeping used for dispatching.
+         *
+         * @internal
+         */
+        _stopDispatching() {
+            this._pendingPayload = null;
+            this._isDispatching = false;
+        }
+
+
+    });
+
+})(Polymer);
